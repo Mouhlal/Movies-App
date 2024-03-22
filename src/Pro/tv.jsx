36099cea426bd96cfa154e7cb86e78ab3b7ccb10
Mouhlal <akramemouhlal@gmail.com>
2024-03-22 16:29:35 +0000

    some change

--- src/Pro/tv.jsx
+++ src/Pro/tv.jsx
@@ -46,7 +46,26 @@ export default function Tv() {
               </CardBody>
               <CardFooter className="pt-0">
                 <Link to={`/detailstv/${ser.id}`}>
-                  <Button color="blue">Details</Button>
+                <Button
+                    variant="text"
+                    className="flex items-center gap-2 bg-black text-white"
+                  >
+                    Read More{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="h-5 w-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                      />
+                    </svg>
+                  </Button>
                 </Link>
               </CardFooter>
             </Card>
